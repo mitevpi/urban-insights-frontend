5f8deb1d21a4ec6d27c16f46d3951ce55c73a65d
Petr Mitev <p.mitevpi@gmail.com>
2019-02-24 13:36:07 -0800

    trying to fix now deploy

--- src/views/ArPaperPage.vue
+++ src/views/ArPaperPage.vue
@@ -2,7 +2,7 @@
   <div>
     <!--<h1 @click='testClick'>TEST COMPONENT OUT SIDE.</h1>-->
     <!-- <h1>This is the page for rendering AR models on a local marker.</h1> -->
-    <ArPaperViewer modelPath="3d/testHouseRotate.obj"></ArPaperViewer>
+    <ArPaperViewer modelPath="/3d/testHouseRotate.obj"></ArPaperViewer>
   </div>
 </template>
 
