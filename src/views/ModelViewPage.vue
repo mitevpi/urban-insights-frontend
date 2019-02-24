a6b4ea55526e1f04f2e1ae7a8dbd532c51ba648f
Petr Mitev <p.mitevpi@gmail.com>
2019-02-24 01:12:01 -0800

    added sample file to basic viewer

--- src/views/ModelViewPage.vue
+++ src/views/ModelViewPage.vue
@@ -1,8 +1,7 @@
 <template>
   <div>
-    <!--<h1>This is the page for rendering models on the phone without AR/VR.</h1>-->
-    <!-- <model-viewer modelPath="../../public/3d/Bloxhub_superclean.obj"></model-viewer> -->
-    <model-viewer modelPath="3d/male02.obj"></model-viewer>
+    <!-- <model-viewer modelPath="3d/male02.obj"></model-viewer> -->
+    <model-viewer modelPath="3d/testHouse.obj"></model-viewer>
   </div>
 </template>
 
