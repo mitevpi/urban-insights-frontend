d9d2d3a0e4d7f63b895e7f3ed2ef2e02858e7d44
Petr Mitev <p.mitevpi@gmail.com>
2019-02-26 15:25:48 -0500

    trying to fix AR

--- src/components/ArPaperViewer.vue
+++ src/components/ArPaperViewer.vue
@@ -1,19 +1,22 @@
 <template>
   <!-- <h1 @click='testClick'>TEST COMPONENT INSIDE.</h1> -->
   <div class="scene3d" style="width:100%; height:100%;">
+    <!-- <a-scene embedded arjs="sourceType: webcam; debugUIEnabled: false"> -->
     <a-scene
-            embedded
-            arjs="sourceType: webcam; detectionMode: mono_and_matrix; matrixCodeType: 3x3; debugUIEnabled: false;"
-            vr-mode-ui="enabled: false"
+      embedded
+      arjs="sourceType: webcam; detectionMode: mono_and_matrix; matrixCodeType: 3x3; debugUIEnabled: false;"
+      vr-mode-ui="enabled: false"
     >
       <a-assets>
-        <!-- <a-asset-item id="city" :src="modelPath"></a-asset-item> -->
-        <a-asset-item id="obj-model" :src="modelPath"></a-asset-item>
+        <a-asset-item id="obj" :src="modelPath"></a-asset-item>
       </a-assets>
 
-      <a-marker-camera preset="custom" type="pattern" url="/3d/pattern-marker.patt">
-        <!-- <a-entity collada-model="#city" position="0 0 0" scale="0.5 0.5 0.5"></a-entity> -->
-        <a-entity obj-model="obj: #obj-model"></a-entity>
+      <!-- <a-marker-camera preset="custom" type="pattern" url="3d/pattern-marker.patt"> -->
+       <a-marker-camera preset='hiro'>
+        <!-- <a-entity obj-model="#obj" position="0 0 0" scale="0.5 0.5 0.5"></a-entity> -->
+        <a-entity 
+            obj-model="obj: url(/3d/testHouseRotate.obj)" position="0 0 0" scale="0.5 0.5 0.5">
+        </a-entity>
       </a-marker-camera>
     </a-scene>
   </div>
@@ -21,16 +24,16 @@
 
 <script>
 
-  export default {
-    name: "ArPaper",
-    components: {},
-    props: {
-      modelPath: String
-    },
-    methods: {
-      testClick() {
-        console.log("MODEL PATH", this.modelPath);
-      }
+export default {
+  name: "ArPaper",
+  components: {},
+  props: {
+    modelPath: String
+  },
+  methods: {
+    testClick() {
+      console.log("MODEL PATH", this.modelPath);
     }
-  };
+  }
+};
 </script>
