e13136d69f584bb75d0d99ea9197c50a21ef32ce
Petr Mitev <p.mitevpi@gmail.com>
2019-03-06 17:13:29 -0500

    cleanup

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,8 @@
   <v-app>
     <v-toolbar app>
       <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
+        <span>Urban</span>
+        <span class="font-weight-light">Insights</span>
       </v-toolbar-title>
       <v-spacer></v-spacer>
       <v-btn
